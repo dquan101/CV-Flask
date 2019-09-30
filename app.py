fc20e58424a981e32d3b1f73536822cea90291ee
Diego <diegoquan101@gmail.com>
2019-09-29 20:39:49 -0600

    initial load

---
+++ app.py
@@ -0,0 +1,31 @@
+import yaml
+from flask import Flask, jsonify, render_template, request
+import os, optparse, sys
+app = Flask(__name__)
+developer = os.getenv("DEVELOPER", "Diego Quan")
+environment = os.getenv("ENVIRONMENT", "development")
+
+
+with open('info.yml') as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", developer=developer)
+
+@app.route("/academica")
+def academica():
+    return render_template("academica.html", developer=developer)
+
+@app.route("/laboral")
+def laboral():
+    return render_template("laboral.html", developer=developer)
+
+if __name__=="__main__":
+    debug=True
+    if environment == 'development' or environment == 'local':
+        debug=True
+    print('Local change')
+    app.run(host="0.0.0.0", debug=debug)
